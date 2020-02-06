acd4458aede7e87e4dccc810304f0c95d30c3c81
Arkanius <vtr.gomes@hotmail.com>
2020-02-05 23:11:48 -0300

    remove ipynb

--- color-tracking.py
+++ color-tracking.py
@@ -15,9 +15,7 @@ if __name__=='__main__':
         lower = np.array([0,220, 220], dtype=np.uint8)  # Amarelo escuro
         upper = np.array([50, 255, 255], dtype=np.uint8)   # Amarelo Claro
 
-        mask = cv2.inRange(frame, lower, upper) # create the mask - InTange Docs: https://docs.opencv.org/3.4/da/d97/tutorial_threshold_inRange.html
-
-        # cv2.imshow('mascara',mask)
+        mask = cv2.inRange(frame, lower, upper) # create the mask - InRange Docs: https://docs.opencv.org/3.4/da/d97/tutorial_threshold_inRange.html
 
         contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # find the countours - Countors doc - https://docs.opencv.org/3.4/d4/d73/tutorial_py_contours_begin.html
 
@@ -28,7 +26,7 @@ if __name__=='__main__':
                 cv2.putText(frame, 'x: '+str(x)+' - y: '+str(y), (x+5,y-5), font,.8, [255,255,255], 1, cv2.LINE_AA)
                 image = frame[y:y+h,x:x+w]
 
-                cv2.imshow('imagem cortada', image)
+                cv2.imshow('pacman', image)
 
         cv2.imshow('frame', frame)
 
