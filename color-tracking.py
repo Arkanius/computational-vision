c6071751a21aa27f310cbf2a301320e2e1f39f7f
Arkanius <vtr.gomes@hotmail.com>
2020-02-05 22:55:04 -0300

    fixc comments

--- color-tracking.py
+++ color-tracking.py
@@ -5,40 +5,34 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 
 if __name__=='__main__':
 
-    cap = cv2.VideoCapture('imagens/pacman.webm')  # Captura Vídeo
+    cap = cv2.VideoCapture('imagens/pacman.webm')
     
     while True:
-        ret,frame = cap.read() # Captura um frame do video
-        if not ret:  # Verifica status do vídeo
+        ret,frame = cap.read()
+        if not ret:
             exit()
 
-        lower = np.array([0,220, 220], dtype=np.uint8)  # (Amarelo escuro) Determina o limite inferior [daqui para cima] 
-        upper = np.array([50, 255, 255], dtype=np.uint8)   # (Amarelo Claro) Determina o limite superior [daqui para baixo]
+        lower = np.array([0,220, 220], dtype=np.uint8)  # Amarelo escuro
+        upper = np.array([50, 255, 255], dtype=np.uint8)   # Amarelo Claro
 
-        mask = cv2.inRange(frame, lower, upper) # Cria máscara a partir dos limites LOWER-UPPER
-        # Documentação sobre InRange: https://docs.opencv.org/3.4/da/d97/tutorial_threshold_inRange.html
+        mask = cv2.inRange(frame, lower, upper) # create the mask - InTange Docs: https://docs.opencv.org/3.4/da/d97/tutorial_threshold_inRange.html
 
         # cv2.imshow('mascara',mask)
 
-        contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # Encontra os contornos na máscara
-        # Documentação sobre Contours: https://docs.opencv.org/3.4/d4/d73/tutorial_py_contours_begin.html
+        contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # find the countours - Countors doc - https://docs.opencv.org/3.4/d4/d73/tutorial_py_contours_begin.html
 
-        
         for cnts in contours:
-            (x,y,w,h) = cv2.boundingRect(cnts) # Cria retângulos com os limites dos contornos
+            (x,y,w,h) = cv2.boundingRect(cnts)
             if w > 25 and h > 25:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 4, cv2.LINE_AA) # imprime o retângulo no Frame
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 4, cv2.LINE_AA)
+                cv2.putText(frame, 'x: '+str(x)+' - y: '+str(y), (x+5,y-5), font,.8, [255,255,255], 1, cv2.LINE_AA)
+                image = frame[y:y+h,x:x+w]
 
-                cv2.putText(frame, 'x: '+str(x)+' - y: '+str(y), (x+5,y-5), font,.8, [255,255,255], 1, cv2.LINE_AA) # Imprime o texto das coordenadas
-                imagem_cortada = frame[y:y+h,x:x+w]
+                cv2.imshow('imagem cortada', image)
 
-                cv2.imshow('imagem cortada', imagem_cortada) # Segmenta e exibe a área da detecção em uma imagem menor
+        cv2.imshow('frame', frame)
 
-        cv2.imshow('frame', frame) # Exibe o resultado
-
-        c = cv2.waitKey(15)    # Aguarda tecla ser pressionada por determinad tempo
-        # documentação sobre WaitKey: https://docs.opencv.org/2.4/modules/highgui/doc/user_interface.html?highlight=waitkey
-
+        c = cv2.waitKey(15)
         if c == ord('q'):
             break
 
